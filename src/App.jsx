af9bf3c141a312aebac915ebe26308fd7e2ee8b5
Mary Cole <adebimpecole02@gmail.com>
2023-07-06 21:08:14 +0100

    added more routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
 import Login from './Authentication Pages/Login'
 import SignUp from './Authentication Pages/SignUp'
+import Dashboard from './Dashboard/Dashboard';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Dashboard/Home';
+import Manage from './Dashboard/Manage';
+import Profile from './Dashboard/Profile';
+import Tickets from './Dashboard/Tickets';
+import CreateEvent from './Dashboard/CreateEvent';
 
 
 function App() {
@@ -10,11 +16,14 @@ function App() {
     <BrowserRouter>
       <>
           <Routes>
-            {/* <Route exact path='*' element={<Home/>}/> */}
+            <Route exact path='/dashboard' element={<Dashboard/>}/>
             <Route exact path='*' element={<Login/>}/>
             <Route exact path='/signup' element={<SignUp/>}/>
-            
-            {/* <Route exact path='/dashb' element={<Dashboard/>}/> */}
+            <Route exact path='/home' element={<Home/>}/>
+            <Route exact path='/manage' element={<Manage/>}/>
+            <Route exact path='/profile' element={<Profile/>}/>
+            <Route exact path='/ticket' element={<Tickets/>}/>
+            <Route exact path='/event' element={<CreateEvent/>}/>
             
             {/* <Route exact path='/cart' element={<Cart/>}/>             */}
           </Routes>
