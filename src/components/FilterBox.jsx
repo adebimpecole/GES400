99083adf3f0cfe91c66efb5d259c24c3a655a867
Mary Cole <adebimpecole02@gmail.com>
2023-08-04 10:49:29 +0100

    change of font style and some minor styling

--- src/components/FilterBox.jsx
+++ src/components/FilterBox.jsx
@@ -1,5 +1,4 @@
 import "../Dashboard/Home.css"
-import date from "../assets/date.svg"
 import location from "../assets/location.svg"
 import { useState } from "react"
 
@@ -42,7 +41,7 @@ const FilterBox = ({array, onFilterChange}) => {
             </datalist>
         </Dropdown>
     
-        <input type="date"/>
+        <input type="date" className="search_date"/>
         {/* <button className='each_filter'><img src={date} className='filter_icon' alt='icon'/><span className='filter_text'>Any date</span></button> */}
         <span className='each_filter'><span className='filter_text'>Top</span></span>
         <span className='each_filter'><span className='filter_text'>Following</span></span>
