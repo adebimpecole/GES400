31a29c586eb4ec08bdea16f783a401e8dcd77bbd
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-08-09 12:51:26 +0100

    feat: working on the action for the edit user page

--- src/actions/authenticationActions.js
+++ src/actions/authenticationActions.js
@@ -24,36 +24,13 @@ export async function signUpAction({request}){
     const formData = await request.formData();
 
     try {
-        // const res = await axios.post(import.meta.env.VITE_SERVER_URL + '/api/auth/local/register', formData)
-        // const {jwt, user : {id , username}} = res.data
-        // if(!id){
-        //     return 'Something went wrong'
-        // }
-        // const img = new Image(prince)
-        const dummyBlob = await fetch(prince).then(res => res.blob())
-        const file = new File([dummyBlob], "capture.png", {
-            type: 'image/png'
-        });
-        formData.append("ref", "plugin::users-permissions.user")
-        formData.append("refId", 1)
-        formData.append("field", "profile")
-        formData.append("files", file)
-        formData.append("files", file)
-        // formData.set("profile", [file, formData.get("profile")])
-
-        const updates = Object.fromEntries(formData);
-        console.log(updates)
-
-        await axios.post(import.meta.env.VITE_SERVER_URL + '/api/upload', formData, {
-            headers:{
-                'Authorization' : 'Bearer ' + getUserFromSession().token
-            }
-        })
-
-
-
-
-        // setUserToSession({token: jwt, id, username})
+        const res = await axios.post(import.meta.env.VITE_SERVER_URL + '/api/auth/local/register', formData)
+        const {jwt, user : {id , username}} = res.data
+        if(!id){
+            return 'Something went wrong'
+        }
+        
+        setUserToSession({token: jwt, id, username})
         return redirect('/dashboard')
     } catch (error) {
         console.log(error)
